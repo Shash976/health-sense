cf37ebaf4042882c544b3996e5830df3fefc4985
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2025-06-17 09:56:37 +0530

    Add OptionsPage for navigation to different modes

---
+++ lib/options.dart
@@ -0,0 +1,69 @@
+import 'package:bio_amp/cvConfigPage.dart';
+import 'package:bio_amp/analyteDashboard.dart';
+import 'package:bio_amp/dpvConfigPage.dart';
+import 'package:bio_amp/tasks.dart';
+import 'package:flutter/material.dart';
+
+class OptionsPage extends StatelessWidget {
+  final String deviceIp;
+
+  const OptionsPage({super.key, required this.deviceIp});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Options'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(padding: EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => TaskPage(
+                      deviceIp: deviceIp
+                    ),
+                  ),
+                );
+              },
+              child: const Text('Analyte Mode'),
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => CVConfigPage(
+                      deviceIp: deviceIp,
+                    ),
+                  ),
+                );
+              },
+              child: const Text('CV Mode'),
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => DPVConfigPage(deviceIp: deviceIp)
+                  ),
+                );
+              },
+              child: const Text('DPV Mode'),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
